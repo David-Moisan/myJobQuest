80cde341ee336b26bcca05f723e2b57b6ac288b5
David-Moisan <dmoisan.23@gmail.com>
2022-04-25 15:36:20 +0200

    Feat: Create navbar fixed #9

---
+++ public/assets/js/index.js
@@ -0,0 +1,13 @@
+let sidebar = document.getElementById('sidebar')
+
+function openNav() {
+    this.sidebar.style.width = "233px"
+    this.sidebar.classList.add('open')
+    this.sidebar.classList.remove('close')
+}
+
+function closeNav() {
+    this.sidebar.style.width = "70px"
+    this.sidebar.classList.add('close')
+    this.sidebar.classList.remove('open')
+}
