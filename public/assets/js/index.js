1a099bace2d1d0a16a95cb7ff693e320a602423a
David-Moisan <dmoisan.23@gmail.com>
2022-04-26 13:02:32 +0200

    Fix: Nav #6

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,13 @@
-let sidebar = document.getElementById('sidebar')
+// let sidebar = document.getElementById('sidebar')
 
-function openNav() {
-    this.sidebar.style.width = "233px"
-    this.sidebar.classList.add('open')
-    this.sidebar.classList.remove('close')
-}
+// function openNav() {
+//     this.sidebar.style.width = "233px"
+//     this.sidebar.classList.add('open')
+//     this.sidebar.classList.remove('close')
+// }
 
-function closeNav() {
-    this.sidebar.style.width = "70px"
-    this.sidebar.classList.add('close')
-    this.sidebar.classList.remove('open')
-}
+// function closeNav() {
+//     this.sidebar.style.width = "70px"
+//     this.sidebar.classList.add('close')
+//     this.sidebar.classList.remove('open')
+// }
