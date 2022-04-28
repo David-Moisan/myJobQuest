9fdcdfd8d3b0d12ef5c998ff2ab213f115367263
David-Moisan <dmoisan.23@gmail.com>
2022-04-28 09:15:40 +0200

    Feat: Add showPassword features in form login #19

--- public/assets/js/showPassword.js
+++ public/assets/js/showPassword.js
@@ -1,13 +1,23 @@
+// Span eyes
+let eye = document.getElementById("show-password")
+
 function handleClick() {
-    addEventListener("click", () => {
-        let eye = document.getElementsByClassName("fa-eye-slash")
-        
-        if (eye.classList.contains('fa-eye-slash')) {
-            eye.classList.replace('fa-eye-slash', 'fa-eye')
-            console.log(eye);
-        } else {
-            eye.classList.replace('fa-eye', 'fa-eye-slash')
-            console.log(eye);
-        }
-    })
+    // Span eyes
+    let eye = document.getElementById("show-password")
+    // Input password
+    let inputType = document.getElementById("inputPassword")
+    //Conditions
+    if (eye.classList.contains('fa-eye-slash')) {
+        eye.classList.replace('fa-eye-slash', 'fa-eye')
+        inputType.type = "text"
+    } else {
+        eye.classList.replace('fa-eye', 'fa-eye-slash')
+        inputType.type = "password"
+    }
 }
+
+const once = {
+    once: true
+}
+
+eye.addEventListener("click", handleClick, once)
